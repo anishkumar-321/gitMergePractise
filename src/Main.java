8429892705985e40683be834f8a27d7a44f5d767
anishkumar-321 <85576349+anishkumar-321@users.noreply.github.com>
2024-10-23 06:39:08 +0530

    Updated to sequence of 6 to 10 Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
+        System.out.println(6);
+        System.out.println(7);
+        System.out.println(8);
+        System.out.println(9);
+        System.out.println(10);
     }
 }
