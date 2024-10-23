98206fe9a775ff76e0e6204af18c2363151c8d2e
anishkumar-321 <85576349+anishkumar-321@users.noreply.github.com>
2024-10-23 06:46:27 +0530

    Update sequence of 11 to 15 Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
+        System.out.println(11);
+        System.out.println(12);
+        System.out.println(13);
+        System.out.println(14);
+        System.out.println(15);
     }
 }
